3f534f6e28f38ad5dcfd6b5d94a55636d7471cc9
dennissiq <ti.dennissiqueira@gmail.com>
2022-10-11 00:49:32 -0300

    feat: create initial interface

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GlobalStyle from './components/GlobalStyle/GlobalStyle';
+import App from './components/sections/App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
+    <GlobalStyle />
     <App />
   </React.StrictMode>
-)
+);
